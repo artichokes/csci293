42d399831b75ab92ba12c60fec2564a2558614a5
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2017-01-19 17:05:37 -0500

    Add Program 2

---
+++ HW2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW2
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            // Get path of file to process
+            Console.Write("Enter name of file containing integers: ");
+            var fileName = Console.ReadLine();
+
+            // Check if file exists
+            if (fileName == null || !System.IO.File.Exists(fileName))
+            {
+                Console.WriteLine("Unable to open file: {0}", fileName);
+                Console.Write("Press Return to exit...");
+                Console.ReadLine();
+                return 1;
+            }
+
+            // Read file contents and split into strings at spaces, tabs, and newlines
+            var fileContents = System.IO.File.ReadAllText(fileName);
+            var fileStrings = fileContents.Split(new char[] {' ', '\t', '\r', '\n'},
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Convert each string into an integer and store in `fileInts[]`
+            var fileInts = Array.ConvertAll(fileStrings, int.Parse);
+
+            // Sort the numbers
+            Array.Sort(fileInts);
+
+            // Print the array fileInts[]
+            Console.WriteLine("------------------------------");
+            Array.ForEach(fileInts, i => Console.WriteLine("{0}", i));
+
+            // Exit the program
+            Console.WriteLine("------------------------------");
+            Console.Write("Press Return to exit...");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
