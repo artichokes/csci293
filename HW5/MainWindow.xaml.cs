1c32bf6236d7df6b71a8dead08ea7d87b4e4b6cd
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2017-03-05 14:20:27 -0500

    Add HW5

---
+++ HW5/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using Action = System.Action;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Window = System.Windows.Window;
+
+namespace HW5
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FilePickerContext _filePickerContext = new FilePickerContext();
+        private readonly ResultsContext _resultsContext = new ResultsContext();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set data contexts
+            FilePicker.DataContext = _filePickerContext;
+            Results.DataContext = _resultsContext;
+        }
+
+        public void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            _resultsContext.Clear(); // Clear existing data
+
+            // Check if file exists
+            if (_filePickerContext.FileName == null || !File.Exists(_filePickerContext.FileName))
+            {
+                DisplayError($"File does not exist: \"{_filePickerContext.FileName}\"");
+            }
+            else
+            {
+                Application excelApp = null;
+                try
+                {
+                    excelApp = new Application {Visible = false};
+                    var workbook = excelApp.Workbooks.Open(_filePickerContext.FileName, ReadOnly: true);
+                    Worksheet worksheet = workbook.Sheets[2]; // Second sheet
+
+                    _resultsContext.ReadSheet(worksheet);
+
+                    workbook.Close(false);
+
+                    Dispatcher.BeginInvoke((Action) (() => MainTabControl.SelectedIndex = 1)); // Switch tab
+                }
+                catch
+                {
+                    DisplayError($"Unable to open file: \"{_filePickerContext.FileName}\"");
+                }
+                finally
+                {
+                    excelApp?.Quit();
+                }
+            }
+        }
+
+        public void PickFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                DefaultExt = ".xlsx",
+                Filter = "Excel Documents (.xlsx) | *.xlsx"
+            };
+            if (openFileDialog.ShowDialog() == true) // If a file is selected
+                _filePickerContext.FileName = openFileDialog.FileName; // Set the text box to the file name selected
+        }
+
+        private void DisplayError(string message)
+        {
+            Success.Visibility = Visibility.Hidden;
+            MessageBox.Show(message, null, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ HW5/ResultsContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Office.Interop.Excel;
+
+namespace HW5
+{
+    public class ResultsContext : INotifyPropertyChanged
+    {
+        public string A1 { get; private set; }
+
+        public string B1 { get; private set; }
+
+        public string C1 { get; private set; }
+
+        public string D1 { get; private set; }
+
+        public string E1 { get; private set; }
+
+        public string F1 { get; private set; }
+
+        public string G1 { get; private set; }
+
+        public string A2 => _a2?.ToShortDateString();
+        public string B2 { get; private set; }
+
+        public string C2 { get; private set; }
+
+        public string D2 { get; private set; }
+
+        public string E2 => Convert.ToInt32(_e2).ToString();
+        public string F2 => _f2?.ToString();
+        public string G2 => _g2?.ToString();
+
+        public void Clear()
+        {
+            A1 = B1 = C1 = D1 = E1 = F1 = G1 = null;
+            _a2 = null;
+            B2 = C2 = D2 = null;
+            _e2 = _f2 = _g2 = null;
+
+            foreach (var cell in _cells)
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(cell));
+        }
+
+        public void ReadSheet(Worksheet worksheet)
+        {
+            A1 = worksheet.Cells[1, 1].Value;
+            B1 = worksheet.Cells[1, 2].Value;
+            C1 = worksheet.Cells[1, 3].Value;
+            D1 = worksheet.Cells[1, 4].Value;
+            E1 = worksheet.Cells[1, 5].Value;
+            F1 = worksheet.Cells[1, 6].Value;
+            G1 = worksheet.Cells[1, 7].Value;
+
+            _a2 = worksheet.Cells[2, 1].Value;
+            B2 = worksheet.Cells[2, 2].Value;
+            C2 = worksheet.Cells[2, 3].Value;
+            D2 = worksheet.Cells[2, 4].Value;
+            _e2 = worksheet.Cells[2, 5].Value;
+            _f2 = worksheet.Cells[2, 6].Value;
+            _g2 = worksheet.Cells[2, 7].Value;
+
+            foreach (var cell in _cells)
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(cell));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime? _a2;
+        private double? _e2, _f2, _g2;
+
+        private readonly string[] _cells =
+        {
+            "A1", "B1", "C1", "D1", "E1", "F1", "G1",
+            "A2", "B2", "C2", "D2", "E2", "F2", "G2"
+        };
+
+        #endregion
+    }
+}
